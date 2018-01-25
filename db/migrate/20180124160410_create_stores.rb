849a23d45335cbe9c883bcaa3e34acb51ef4b43d
Roman704 <ro1990ma@mail.ru>
2018-01-25 16:19:36 +0200

    MAke it better with heroku

--- db/migrate/20180124160410_create_stores.rb
+++ db/migrate/20180124160410_create_stores.rb
@@ -1,8 +1,15 @@
 class CreateStores < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :stores do |t|
-
+      t.string :title
+      t.string :description
+      t.string :price
+      t.integer :category
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :stores
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707111715) do
+ActiveRecord::Schema.define(version: 20180124160410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "price"
+    t.integer  "category"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
